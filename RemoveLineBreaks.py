65a870142dd0523853c44109b028c077eea864de
zhang96 <jzhan264@uottawa.ca>
2016-03-28 16:23:11 -0400

    Made it look better.

--- RemoveLineBreaks.py
+++ RemoveLineBreaks.py
@@ -19,16 +19,21 @@ if not os.path.isfile(output_file_name):
     os.system(output_file_name)
     print'File named %s created' % output_file_name
     write()
+
 else:
+
     if os.stat(output_file_name).st_size == 0:
         print'File %s is empty' %output_file_name
         write()
+
     else:
         input = (raw_input('There is some data in %s, are you sure to delete your existing data in %s? (y/n)' % (output_file_name,output_file_name)))
+
         if input =='y':
             print'Emptying the existing file...'
             open(output_file_name, 'w').close()
             print'File %s is now empty.' % output_file_name
             write()
+
         else:
             print 'Program teminated, user refuse to delete data in %s' % output_file_name
